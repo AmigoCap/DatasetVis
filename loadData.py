1c5369d0453bc3174783e47d6e0fa77162c85bfa
Louis Kraemer <louiskraemer@gmail.com>
2018-02-13 10:57:44 +0100

    Resize function convert all images in a folder

--- loadData.py
+++ loadData.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from os import listdir
+from os.path import isfile, join
 
 
 def loadImage(filePath):
@@ -7,8 +9,9 @@ def loadImage(filePath):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return image
 
-def resize_image(filePath, final_height, final_width):
-    image = loadImage(filePath)
+
+def resize_image(folderPath, fileName, final_height, final_width):
+    image = loadImage(folderPath + '/' + fileName)
     height, width, channels = image.shape
     final_ratio = float(final_width) / final_height
     ratio = float(width / height)
@@ -21,7 +24,7 @@ def resize_image(filePath, final_height, final_width):
     elif ratio > final_ratio:
         wanted_height = round(width/final_ratio)
         border = int(wanted_height - height)
-        blank_image = np.zeros((border, height, 3), np.uint8)
+        blank_image = np.zeros((border, width, 3), np.uint8)
         blank_image[:, :] = (255, 255, 255)
         final = np.concatenate((image, blank_image), axis=0)
     else:
@@ -30,4 +33,11 @@ def resize_image(filePath, final_height, final_width):
     # cv2.imshow('Resized Image', resized_image)
     # cv2.waitKey(0)
 
-    return resized_image
+    return cv2.imwrite('data/' + fileName, resized_image, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+
+
+def resize_dataset(folderPath, height, width):
+    images = [f for f in listdir(folderPath) if isfile(join(folderPath, f))]
+    for image in images:
+        if not resize_image(folderPath, image, height, width):
+            print(image + ' no success on resize')
