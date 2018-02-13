10984edf36dbc9fa9fc54d757e700fa7b853752d
Louis Kraemer <louiskraemer@gmail.com>
2018-02-13 11:04:26 +0100

    main.py updated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import loadData as ld
 
-ld.resize_image('./data/image/bar-chart.png', 160, 120)
+# Enter the local folder path of the dataset to convert it into jpeg with the desired height and width, the output will be in the data folder
+ld.resize_dataset('/home/louis/Documents/Dev/Dataset', 32, 32)
